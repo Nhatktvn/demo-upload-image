4d12555ff5bbb33298b990caa9483e0a08378605
minhnhat <nguyenthiminhnguyetkt9x1@gmail.com>
2023-10-09 00:45:23 +0700

    update

--- src/main/java/com/minhnhat/uploadimage/controller/ImageController.java
+++ src/main/java/com/minhnhat/uploadimage/controller/ImageController.java
@@ -4,10 +4,13 @@ import com.minhnhat.uploadimage.entity.Image;
 import com.minhnhat.uploadimage.service.ImageService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
